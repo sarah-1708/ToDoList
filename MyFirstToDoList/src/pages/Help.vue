3bc4e9e53ea279c47ff1b6d409d32587a614ca87
Sarah Rambaud <52890781+sarah-1708@users.noreply.github.com>
2022-05-11 08:51:06 +0200

    Add files via upload

---
+++ MyFirstToDoList/src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg" >
+    <h5 class="q-mt-none">Aide</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum urna sit amet ipsum dapibus, a tincidunt mauris ornare. Etiam tortor nunc, cursus sit amet enim et, luctus finibus felis. Nullam ultrices nisi nec tempus accumsan. Duis lobortis turpis id congue fringilla. Cras lacus justo, finibus nec risus non, sollicitudin sodales risus. In semper sed felis blandit mollis. Nulla ornare quis tortor vel placerat. Maecenas et ligula massa. Nunc pellentesque nibh quis elit tincidunt mollis at at nibh. Morbi finibus mauris a elit eleifend, sed ornare orci rutrum. Proin tincidunt eu dolor ornare aliquam.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum urna sit amet ipsum dapibus, a tincidunt mauris ornare. Etiam tortor nunc, cursus sit amet enim et, luctus finibus felis. Nullam ultrices nisi nec tempus accumsan. Duis lobortis turpis id congue fringilla. Cras lacus justo, finibus nec risus non, sollicitudin sodales risus. In semper sed felis blandit mollis. Nulla ornare quis tortor vel placerat. Maecenas et ligula massa. Nunc pellentesque nibh quis elit tincidunt mollis at at nibh. Morbi finibus mauris a elit eleifend, sed ornare orci rutrum. Proin tincidunt eu dolor ornare aliquam.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
